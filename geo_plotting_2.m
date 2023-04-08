bc80b4dbe68c90552ad67819403e071c225fe920
Joao Jesus <joaomj1800@gmail.com>
2023-04-08 18:42:02 +0100

    update

--- geo_plotting_2.m
+++
@@ -1,9 +0,0 @@
-% Create a new figure window.
-figure('Name', 'Longitude and Latitude Plot')
-
-% Add a geographic axes to the figure window.
-
-geoshow(lat,lon,data)
-
-% Add a title and labels to the plot.
-title('Longitude and Latitude Plot')
